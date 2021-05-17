3c7e7493ffa8dfdbde31bb8a57dc5fc4a601bab7
Ethan <ethan.blumenthal@gmail.com>
2021-05-17 11:11:00 -0500

    Created orderbook mapping in DEX contract.

---
+++ contracts/dex.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.8.4;
+
+import "./wallet.sol";
+
+contract Dex is Wallet {
+    enum Side {
+        BUY,
+        SELL
+    }
+
+    struct Order {
+        uint id;
+        address trader;
+        Side side;
+        bytes32 ticker;
+        uint amount;
+        uint price;
+    }
+
+    mapping(bytes32 => mapping(uint => Order[])) public orderBook;
+
+    function getOrderBook(bytes32 ticker, Side side) view public returns(Order[] memory) {
+        return orderBook[ticker][uint(side)];
+    }
+
+    function createLimitOrder() {
+
+    }
+}
